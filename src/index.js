ad821a69ccdf99a37e8068c888881ff8fd0822fc
David Ontiveros Flores <David@MacBook-Pro-de-David.local>
2021-07-11 22:31:57 -0500

    Js complete , CCS start

--- src/index.js
+++ src/index.js
@@ -1,34 +1,41 @@
 /* eslint-disable no-console */
-import validator from './validator.js';
-console.log(validator);
+import validator from "./validator.js";
+//console.log(validator);
 
-const boton =document.getElementById('validar');
+const boton = document.getElementById("validar");
 
- boton.addEventListener ("click",function(){
-  const CreditCardNumber=document.getElementById("cardNumber").value;
-  console.log("Tu tarjeta es",CreditCardNumber);
-
-  //var numerica= parseInt(reverseString(CreditCardNumber));
-
-  //console.log(numerica);
-  console.log('Tu tarjeta tiene ' + CreditCardNumber.length+ " digitos");
-  validator.isValid(CreditCardNumber);
-  validator.maskify(CreditCardNumber);
-  
-  var noValido= / /;
-  if(noValido.test(CreditCardNumber)){ // se chequea el regex de que el string no tenga espacio
-    alert ("La contraseña no puede contener espacios en blanco"); 
-   return true; 
-}
-else{ 
-   return false; 
-}
-
-
-
+boton.addEventListener("click", function () {
+  const CreditCardNumber = document.getElementById("cardNumber").value;
+  const resultado = validator.isValid(CreditCardNumber);
+  document.getElementById("first").style.display="none"
+  const hash = validator.maskify(CreditCardNumber);
+  //console.log(resultado, hash);
+  document.getElementById("second").style.display = "block"
+  if (resultado == true) {
+    document.getElementById("cardNumber").value = hash; //value por inner
+    document.getElementById("message").innerHTML = "Tu tarjeta es valida";
+    document.getElementById("lastnumber").innerHTML = hash;
+    console.log(hash);
+  } else {
+    document.getElementById("cardNumber").value = hash;
+    console.log("error");
+    document.getElementById("message").innerHTML = "Tu tarjeta No es valida";
+    document.getElementById("lastnumber").innerHTML = hash; //agregado despues de monse
+  }
 });
+ //secondBoton.addEventListener("click", function(){
+ //  document.getElementById("second").style.display="none";
+  // document.getElementById("first").style.display="block";
+   //document.getElementById("validarOtra").styledisplay="none";
+ //});
 
+//console.log("Tu tarjeta es",CreditCardNumber);
 
+//var numerica= parseInt(reverseString(CreditCardNumber));
 
+//console.log(numerica);
+//console.log('Tu tarjeta tiene ' + CreditCardNumber.length+ " digitos");
+//validator.isValid(CreditCardNumber);
+//validator.maskify(CreditCardNumber);
 
- /*Function{validator.nombredelafuncion*/
+/*Function{validator.nombredelafuncion*/
--- src/validator.js
+++ src/validator.js
@@ -1,77 +1,72 @@
 /* eslint-disable no-console */
 
-
 const validator = {
-  
-     
-    isValid: function(str){
+  isValid: function (str) {
+    var str_reverse = str.split("").reverse().join(""); // Hace split, reverce y vuelve a unir al string CreditNumber
+    var pares = [];
+    var impares = [];
 
-      var str_reverse = str.split("").reverse().join(""); // Hace split, reverce y vuelve a unir al string CreditNumber
-      var pares = []; 
-      var impares = [];
+    for (var i = 0, len = str_reverse.length; i < len; i += 1) {
+      //sacamos la longitud del string y damos la orden del bucle
 
-      for (var i = 0, len = str_reverse.length; i < len; i += 1) { //sacamos la longitud del string y damos la orden del bucle
-
-        if ((i%2) == 0){ //si el residuo  de 2 de la posicion es 0 lo tomara como impar , considerando que el indice del primer elemento es 0
-          impares.push(+str_reverse.charAt(i));
-        }
-        if(i%2!=0){
-          pares.push(+str_reverse.charAt(i)); //si el residuo de 2 de la posicion es diferente a 0 lo tomara como par
-        }
-
+      if (i % 2 == 0) {
+        //si el residuo  de 2 de la posicion es 0 lo tomara como impar , considerando que el indice del primer elemento es 0
+        impares.push(+str_reverse.charAt(i));
+      }
+      if (i % 2 != 0) {
+        pares.push(+str_reverse.charAt(i)); //si el residuo de 2 de la posicion es diferente a 0 lo tomara como par
+      }
     }
-       let sumaimpares = 0;
-       let numimp = 0;  
+    let sumaimpares = 0;
+    let numimp = 0;
 
-    for (i = 0, numimp = 0; i < impares.length; i++) { //sacara la longitud de los impares
-      numimp = impares[i];  //guardo numimp en el arreglo impares en la posicion i
+    for (i = 0, numimp = 0; i < impares.length; i++) {
+      //sacara la longitud de los impares
+      numimp = impares[i]; //guardo numimp en el arreglo impares en la posicion i
       sumaimpares += numimp; //sumaimpares=sumaimpares+numero impar
     }
 
-
-     for (i = 0; i < pares.length; i++) { //sacara la longitud de los pares
-       pares[i]=pares[i]*2; //los pares se multiplicaran por 2
-       
-     }
-      
-     let numpar = 0;
-     let sumapares = 0;
-     for(i=0; i<pares.length;i++){
-       if(pares[i]>9){ // verifica si tiene 2 digitos
-         numpar=pares[i];
-         pares[i]=numpar-9;//le resta 9 para sacar la suma de los 2 digitos, 12=3 12-9=3
-       }
-       numpar=pares[i];
-       sumapares+=numpar;
-     }
-     
-     let total = sumapares+sumaimpares;
-    
-     let resultado=total%10 //Residuo de 10
-     if(resultado==0){
-       return true;
-     } else{
-        return false;
-      }
-  
-    },
-
-    maskify:function (str){  //para ocultar los digitos menos los ultimos 4
-      let longitud=str.length
-      if(longitud<=4){
-        return str;
-      }else{
-        let listado=Array.from(str)
-        for(let i=0; i<str.length-4; i++){
-          listado[i]="#"
-        }
-        let final=listado.join("")
-        return final;
-      }
+    for (i = 0; i < pares.length; i++) {
+      //sacara la longitud de los pares
+      pares[i] = pares[i] * 2; //los pares se multiplicaran por 2
     }
-  
-     }
-      
 
+    let numpar = 0;
+    let sumapares = 0;
+    for (i = 0; i < pares.length; i++) {
+      if (pares[i] > 9) {
+        // verifica si tiene 2 digitos
+        numpar = pares[i];
+        pares[i] = numpar - 9; //le resta 9 para sacar la suma de los 2 digitos, 12=3 12-9=3
+      }
+      numpar = pares[i];
+      sumapares += numpar;
+    }
+
+    let total = sumapares + sumaimpares;
+
+    let resultado = total % 10; //Residuo de 10
+    if (resultado == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+
+  maskify: function (str) {
+    //para ocultar los digitos menos los ultimos 4
+    let longitud = str.length;
+    if (longitud <= 4) {
+      return str;
+    } else {
+      let listado = Array.from(str);
+      for (let i = 0; i < str.length - 4; i++) {
+        listado[i] = "#";
+      }
+      let final = listado.join("");
+      return final;
+    }
+  },
+};
 
 export default validator;
