b0d40b60019358b9d64fc5eb18615fcdfeb9e5e6
David Ontiveros Flores <David@MacBook-Pro-de-David.local>
2021-07-14 23:12:10 -0500

    Final Test

--- src/index.js
+++ src/index.js
@@ -1,37 +1,37 @@
-
 import validator from "./validator.js";
-const boton = document.getElementById("validar"); //primer boton
-const secondBoton = document.getElementById("register2"); //segundo boton
-boton.addEventListener("click", function () {
-  const CreditCardNumber = document.getElementById("cardNumber").value; //cardNumber =id del input
-  if (CreditCardNumber.length < 13) { //restriccion de cantidad de digitos de la tarjeta
-    alert("Favor de introducir un numero de tarjeta, minimo 13 digitos"); // alerta 
-  } else {
-    const resultado = validator.isValid(CreditCardNumber); // resultado del algoritmo
-    document.getElementById("first").style.display = "none" //primer pantalla ,none:oculta 
-    const hash = validator.maskify(CreditCardNumber); // maskify=enmascaramiento de digitos
-
-    document.getElementById("second").style.display = "block" // segunda pantalla, block: muestra
-    if (resultado == true) { //condicional , si es true realiza lo siguiente
-      document.getElementById("cardNumber").value = hash;//oculta el numero de tarjeta en input al regresar a pantalla 1
-      document.getElementById("message").innerHTML = `<span class="valid">Tarjeta valida </spam>`;// usamos span al tener un solo id message en HTML.
-      document.getElementById("imagenValidation").style.display = "block" // imagen palomita verde block:muestra
-      document.getElementById("imagenValidation2").style.display = "none" //imagen tacha roja none: oculta
+const boton = document.getElementById("validar"); //primer boton,
+const secondBoton = document.getElementById("register2"); //segundo boton,
+ boton.addEventListener("click", function () {
+   const CreditCardNumber = document.getElementById("cardNumber").value; //cardNumber =id del input
+  if (CreditCardNumber.length < 13) {
+    //restriccion de cantidad de digitos de la tarjeta
+    alert("Favor de introducir un numero de tarjeta, minimo 13 digitos"); // alerta
+   } else {
+    const resultado = validator.isValid(CreditCardNumber); //  algoritmo
+    document.getElementById("first").style.display = "none"; //primer pantalla ,none:oculta
+     const hash = validator.maskify(CreditCardNumber); // maskify=enmascaramiento de digitos
+    document.getElementById("second").style.display = "block"; // segunda pantalla, block: muestra
+    if (resultado == true) {
+      //condicional , si es true realiza lo siguiente
+      document.getElementById("cardNumber").value = hash; //oculta el numero de tarjeta en input al regresar a pantalla 1
+      document.getElementById(
+        "message"
+      ).innerHTML = `<span class="valid">Tarjeta valida </spam>`; // usamos span al tener un solo id message en HTML.
+      document.getElementById("imagenValidation").style.display = "block"; // imagen palomita verde block:muestra
+      document.getElementById("imagenValidation2").style.display = "none"; //imagen tacha roja none: oculta
       document.getElementById("lastnumber").innerHTML = hash; //el numero de la tarjeta introducida con los # excepto los ultimos 4 digitos
-
-
-    } else {
-      document.getElementById("cardNumber").value = hash;//oculta el numero de tarjeta en input al regresar a pantalla 1
+     } else {
+      document.getElementById("cardNumber").value = hash; //oculta el numero de tarjeta en input al regresar a pantalla 1
       document.getElementById("message").innerHTML = `<span class="invalid">Tarjeta Invalida</spam>`; //usamos span al tener un solo id message en HTML.
-      document.getElementById("imagenValidation").style.display = "none" // imagen palomita verde none:oculta
-      document.getElementById("imagenValidation2").style.display = "block"//imagen tacha roja block: muestra
+      document.getElementById("imagenValidation").style.display = "none"; // imagen palomita verde none:oculta
+      document.getElementById("imagenValidation2").style.display = "block"; //imagen tacha roja block: muestra
       document.getElementById("lastnumber").innerHTML = hash; //el numero de la tarjeta introducida con los # excepto los ultimos 4 digitos
-
     }
   }
 });
-secondBoton.addEventListener("click", function () { //segundo boton para registrar otra tarjeta, manda a primer pantalla
+secondBoton.addEventListener("click", function () {
+  //segundo boton para registrar otra tarjeta, manda a primer pantalla
   document.getElementById("second").style.display = "none"; //id: second de HTML (segunda pantalla)  none: oculta
   document.getElementById("first").style.display = "block"; //id: fisrt de HTML  (primer pantalla)  block: muestra
-  document.getElementById("lastnumber").style.display = "block";//el numero de la tarjeta introducida con los # excepto los ultimos 4 digitos
+  document.getElementById("lastnumber").style.display = "block"; //el numero de la tarjeta introducida con los # excepto los ultimos 4 digitos
 });
--- src/validator.js
+++ src/validator.js
@@ -1,20 +1,19 @@
-/* eslint-disable no-console */
-
 const validator = {
-  isValid: function (str) {
-    var str_reverse = str.split("").reverse().join(""); // Hace split, reverce y vuelve a unir al string CreditNumber
-    var pares = []; //Array
-    var impares = []; //Array
+  isValid: function (CreditCardNumber) {
+    let str_reverse = CreditCardNumber.split("").reverse().join(""); // Hace split, reverce y vuelve a unir al string CreditNumber
+    let pares = []; //Array
+    let impares = []; //Array
 
     for (var i = 0, len = str_reverse.length; i < len; i += 1) {
       //sacamos la longitud del string  (numero de tarjeta) y damos la orden de iteracion
 
       if (i % 2 == 0) {
         //si el residuo de 2 de la posicion es 0 lo tomara como impar , considerando que el indice del primer elemento es 0
-        impares.push(+str_reverse.charAt(i)); //anhade elementos que cumplen con la restriccion,solo impares , nuevo array 
+        impares.push(+str_reverse.charAt(i)); //anhade elementos que cumplen con la restriccion,solo impares , nuevo array
       }
-      if (i % 2 != 0) { //si el residuo de 2 de la posicion es diferente a 0 lo tomara como par
-        pares.push(+str_reverse.charAt(i)); //anhade elementos que cumplen con la restriccion,solo pares , nuevo array 
+      if (i % 2 != 0) {
+        //si el residuo de 2 de la posicion es diferente a 0 lo tomara como par
+        pares.push(+str_reverse.charAt(i)); //anhade elementos que cumplen con la restriccion,solo pares , nuevo array
       }
     }
     let sumaimpares = 0;
@@ -33,36 +32,44 @@ const validator = {
 
     let numpar = 0;
     let sumapares = 0;
-    for (i = 0; i < pares.length; i++) { //sacara la longitud de los pares
-      if (pares[i] > 9) { //verificara si los pares son digitos menor a 9, verifica si tienen 2 digitos
-        numpar = pares[i];//guardo numpar  de los mayores a 9 en el arreglo pares en la posicion i
+    for (i = 0; i < pares.length; i++) {
+      //sacara la longitud de los pares
+      if (pares[i] > 9) {
+        //verificara si los pares son digitos menor a 9, verifica si tienen 2 digitos
+        numpar = pares[i]; //guardo numpar  de los mayores a 9 en el arreglo pares en la posicion i
         pares[i] = numpar - 9; //le resta 9 para sacar la suma de los 2 digitos, 12=3 12-9=3
       }
-      numpar = pares[i];//guardo numpar de los menores a 9 en el arreglo pares en la posicion i
-      sumapares += numpar;//sumapares=sumapares+numero par
+      numpar = pares[i]; //guardo numpar de los menores a 9 en el arreglo pares en la posicion i
+      sumapares += numpar; //sumapares=sumapares+numero par
     }
 
     let total = sumapares + sumaimpares;
 
     let resultado = total % 10; //Residuo de 10
-    if (resultado == 0) { //si el residuo de 10 es 0 
+    if (resultado == 0) {
+      //si el residuo de 10 es 0
       return true; //retorna true
-    } else { //si no
+    } else {
+      //si no
       return false; // retorna false
     }
   },
 
-  maskify: function (str) { // maskify=enmascaramiento de digitos , uso de # excepto en los ultimos 4 digitos
+  maskify: function (CreditCardNumber) {
+    // maskify=enmascaramiento de digitos , uso de # excepto en los ultimos 4 digitos
 
-    let longitud = str.length; //longitud del string
-    if (longitud <= 4) { //si la longitud es menor a 4
-      return str; //retorna tal cual el string
-    } else {//si no
-      let listado = Array.from(str); //
-      for (let i = 0; i < str.length - 4; i++) { // realiza la iteracion hasta 4 digitos antes de que termine el string
+    let longitud = CreditCardNumber.length; //longitud del string
+    if (longitud <= 4) {
+      //si la longitud es menor a 4
+      return CreditCardNumber; //retorna tal cual el string
+    } else {
+      //si no
+      let listado = Array.from(CreditCardNumber); //
+      for (let i = 0; i < CreditCardNumber.length - 4; i++) {
+        // realiza la iteracion hasta 4 digitos antes de que termine el string
         listado[i] = "#"; //sustituye por # los digitos de la iteracion
       }
-      let final = listado.join("");// al final une los caracteres
+      let final = listado.join(""); // al final une los caracteres
       return final;
     }
   },
